90bd8d0e42d1e75532cec9fdff660ed75da6d258
David Provenzano <dprovenzano13@gmail.com>
2022-03-04 22:02:02 -0500

    Test user input

--- dbzWebsite/api-template/js/main.js
+++ dbzWebsite/api-template/js/main.js
@@ -1,5 +1,6 @@
 document.querySelector('button').addEventListener('click', getCharacter)
 
 function getCharacter(){
-    
+    const characterName = document.querySelector('input').value
+    console.log(characterName)
 }
