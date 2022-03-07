22e89d569b3251d5c65d187693233796568daaba
David Provenzano <dprovenzano13@gmail.com>
2022-03-06 20:49:33 -0500

    Add images to assets folder

--- dbzWebsite/api-template/js/main.js
+++ dbzWebsite/api-template/js/main.js
@@ -1,7 +1,7 @@
 document.querySelector('button').addEventListener('click', getCharacter)
 
 function getCharacter(){
-    const characterName = document.querySelector('input').value.toLowerCase().replace(' ', '')
+    const characterName = document.querySelector('input').value.toLowerCase().replace(/\s+/g, '')
     console.log(characterName)
-
+    document.querySelector('img').src = `assets/${characterName}.png`
 }
