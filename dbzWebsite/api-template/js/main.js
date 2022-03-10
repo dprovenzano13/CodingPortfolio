67519b7e37f7596f08299e265e43ec47c4229fed
David Provenzano <dprovenzano13@gmail.com>
2022-03-09 21:40:18 -0500

    Create function to populate drop down

--- dbzWebsite/api-template/js/main.js
+++ dbzWebsite/api-template/js/main.js
@@ -1,7 +1,18 @@
-document.querySelector('button').addEventListener('click', getCharacter)
+// document.querySelector('button').addEventListener('click', getCharacter)
 
-function getCharacter(){
-    const characterName = document.querySelector('input').value.toLowerCase().replace(/\s+/g, '')
-    console.log(characterName)
-    document.querySelector('img').src = `assets/${characterName}.png`
+// function getCharacter(){
+//     const characterName = document.querySelector('input').value.toLowerCase().replace(/\s+/g, '')
+//     console.log(characterName)
+//     document.querySelector('img').src = `assets/${characterName}.png`
+// }
+
+function addOptions(obj){
+    for(let i = 0; i < obj.length; i++){
+        const select = document.querySelector('select')
+        const option = document.createElement('option')
+        option.value = obj[i].name.toUpperCase()
+        option.innerText = obj[i].name.toUpperCase()
+        select.appendChild(option)
+    }
 }
+addOptions(dbs)
