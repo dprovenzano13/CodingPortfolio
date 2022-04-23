0d5be8f713fff9b6ac7d6cc252b03a9edce0126e
David Provenzano <dprovenzano13@gmail.com>
2022-04-23 19:52:07 -0400

    Change image and add comments to JS file

--- TwitchBackgroundChanger/js/main.js
+++ TwitchBackgroundChanger/js/main.js
@@ -6,21 +6,24 @@ document.querySelector('#red').addEventListener('click', partyRed);
 
 function partyPurple () {
   let body = document.querySelector('body');
-  body.style.backgroundColor = 'rgba(241,63,247,1)'
+  body.style.backgroundColor = 'rgba(229, 164, 232, 1)'
   body.style.color = 'white'
 }
 function partyGreen () {
   let body = document.querySelector('body');
-  body.style.backgroundColor = 'rgba(0,253,81,1)'
+  body.style.backgroundColor = 'rgba(0, 210, 67,1)'
   body.style.color = 'white'
 }
 function partyBlue () {
   let body = document.querySelector('body');
-  body.style.backgroundColor = 'rgba(0,254,255)'
+  body.style.backgroundColor = 'rgba(47, 231, 231, 1)'
   body.style.color = 'white'
 }
 function partyRed () {
   let body = document.querySelector('body');
-  body.style.backgroundColor = 'rgba(224,108,117,1)'
+  body.style.backgroundColor = 'rgba(255, 62, 78,1)'
   body.style.color = 'white'
 }
+
+
+// When you click on the color.. The background image changes to a streamer room with that color.
