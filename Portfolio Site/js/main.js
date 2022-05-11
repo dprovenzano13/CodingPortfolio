5f61fdff8074aadb937d8ea2e8c43021ab36af2c
David Provenzano <dprovenzano13@gmail.com>
2022-05-11 11:41:18 -0400

    Change image links for Restaurant website

--- Portfolio Site/js/main.js
+++ Portfolio Site/js/main.js
@@ -3,3 +3,10 @@ const hamburger = document.querySelector('.hamburger')
 hamburger.addEventListener('click', function () {
     this.classList.toggle('isActive')
 })
+
+const nav = document.querySelector('.navLinks')
+
+nav.addEventListener('click',  (e) => {
+    e.target.classList.toggle('isActive')
+    
+})
