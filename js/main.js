39d51d56caa42f8ebaf86a8aa79d4a87612a2969
David Provenzano <dprovenzano13@gmail.com>
2022-05-18 23:27:55 -0400

    Add hamburger nav menu

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
 const hamburger = document.querySelector('.hamburger')
+const hamburgerNav = document.querySelector('.hamburgerNav')
+const hambgurgerNavLink = document.querySelectorAll('.hamburgerNav a')
 
 hamburger.addEventListener('click', function () {
-    this.classList.toggle('isActive')
+    hamburger.classList.toggle('isActive')
+    hamburgerNav.classList.toggle('isActive')
 })
-
 const nav = document.querySelectorAll('.navLinks a')
 
 nav.forEach(link =>{
     link.addEventListener('click', function () {
         nav.forEach(anchor => anchor.classList.remove('isActive'))
         this.classList.add('isActive')
+    })
+})
+hambgurgerNavLink.forEach(link =>{
+    link.addEventListener('click', function () {
+        hamburger.classList.toggle('isActive')
+        hamburgerNav.classList.toggle('isActive')
     })
 })
